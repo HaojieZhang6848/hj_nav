9cc1c1a09e525d16dadf8cfb5c3e808a4c94d4a1
Haojie Zhang <zhj630985214@gmail.com>
2024-03-10 01:56:35 +0800

    nav_http_server添加获得小车是否正在运动的接口

--- nav_http_server/nav_http_server/nav_http_server.py
+++ nav_http_server/nav_http_server/nav_http_server.py
@@ -11,6 +11,7 @@ try:
 except:
     from utils import eular_to_quaternion  # for python3 nav_http_server.py
 import logging
+from threading import Thread, Semaphore
 
 # 设置日志级别和格式
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -37,9 +38,11 @@ class SendNavGoalNode(rclpy.node.Node):
             self.get_logger().error('NavigateToPose action server not available after waiting')
             exit(1)
         self.last_goal = None
+        self.is_moving = False
+        self.lock = Semaphore(1)
+        self.executor = MultiThreadedExecutor(num_threads=3)
 
     def send_nav_goal(self, x, y, theta):
-
         # 如果多次以相同的参数调用send_nav_goal，则不会发送新的目标
         if self.last_goal is not None:
             x_last, y_last, theta_last = self.last_goal
@@ -62,17 +65,39 @@ class SendNavGoalNode(rclpy.node.Node):
         goal_msg.pose.pose.orientation.w = q[3]
 
         goal_handle_future = self.action_client.send_goal_async(goal_msg)
-        rclpy.spin_until_future_complete(self, goal_handle_future, executor=MultiThreadedExecutor(num_threads=3))
+        
+        # 这里的lock的目的是：当导航被抢占(前一个导航还没结束，又有新的导航请求)时
+        # 确保旧的导航将is_moving设置为False后，新的导航才将is_moving设置为True
+        # 如果上述两个过程的顺序颠倒，那么is_moving的值会错误
+        self.lock.acquire()
+        
+        rclpy.spin_until_future_complete(self, goal_handle_future, executor=self.executor)
         goal_handle = goal_handle_future.result()
 
         if not goal_handle.accepted:
             self.get_logger().error('Goal rejected')
             return False, 'goal rejected'
+        
+        self.get_logger().info('Move started')
+        self.is_moving = True
+        
+        def wait_move_stop():
+            rsp_f = goal_handle.get_result_async()
+            rclpy.spin_until_future_complete(self, rsp_f, executor=self.executor)
+            self.get_logger().info('Move stopped')
+            self.is_moving = False
+            self.lock.release()
+            
+        t = Thread(target=wait_move_stop)
+        t.start()
+        
         return True, 'goal accepted'
 
 
 rclpy.init()
 node = SendNavGoalNode()
+# t = Thread(target=node.executor.spin)
+# t.start()
 
 app = Flask(__name__)
 
@@ -94,6 +119,10 @@ def nav():
     else:
         return jsonify({'status': 'error', 'message': message})
 
+@app.route('/is_moving', methods=['GET'])
+def is_moving():
+    # 返回机器人是否在移动
+    return jsonify({'is_moving': node.is_moving})
 
 def main():
     port = 5000
