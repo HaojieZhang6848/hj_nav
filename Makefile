c693949f7325fe76b904b5062bc034bed1dcb82c
Haojie Zhang <zhj630985214@gmail.com>
2024-07-31 17:21:34 +0800

    update x3 0.0.11

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 # limo-dockerx-build-push:
 # 	docker buildx build --platform linux/arm64 -t huajuan6848/limo-54demo-ros-foxy:0.0.9-5 -f docker/limo/Dockerfile --push .
 x3-pi-dockerx-build-push:
-	docker buildx build --platform linux/arm64 -t huajuan6848/x3-54demo-ros-foxy:0.0.10-pi -f docker/x3/Dockerfile.pi --push .
+	docker buildx build --platform linux/arm64 -t huajuan6848/x3-54demo-ros-foxy:0.0.11-pi -f docker/x3/Dockerfile.pi --push .
 x3-jetson-dockerx-build-push:
-	docker buildx build --platform linux/arm64 -t huajuan6848/x3-54demo-ros-foxy:0.0.10-jetson -f docker/x3/Dockerfile.jetson --push .
+	docker buildx build --platform linux/arm64 -t huajuan6848/x3-54demo-ros-foxy:0.0.11-jetson -f docker/x3/Dockerfile.jetson --push .
 x3-base-dockerx-build-push:
-	docker buildx build --platform linux/arm64 -t huajuan6848/x3_ros2:0.0.1 -f docker/x3/Dockerfile.base --push .
+	docker buildx build --platform linux/arm64 -t huajuan6848/x3_ros2:0.0.1-orig-nav -f docker/x3/Dockerfile.base --push .
--- nav_http_server/nav_http_server/nav_http_server.py
+++ nav_http_server/nav_http_server/nav_http_server.py
@@ -17,6 +17,11 @@ from threading import Thread
 from flask_cors import CORS
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from action_msgs.msg import GoalStatus
+import tf2_ros
+try:
+    from .utils import quaternion_to_eular  # for ros2 run
+except:
+    from utils import quaternion_to_eular  # for python3 nav_http_server.py
 
 # 设置日志级别和格式
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -25,9 +30,11 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 parser = ArgumentParser()
 parser.add_argument('--map-frame', default='map')
 parser.add_argument('--navigate-to-pose-action', default='/navigate_to_pose')
+parser.add_argument('--robot-frame', default='base_link')
 args, unknown = parser.parse_known_args()
 
 MAP_FRAME = args.map_frame
+ROBOT_FRAME = args.robot_frame
 NAVIGATE_TO_POSE_ACTION = args.navigate_to_pose_action
 
 DIST_THRESHOLD = 0.01
@@ -41,9 +48,10 @@ class SendNavGoalNode(rclpy.node.Node):
         if not self.action_client.wait_for_server(timeout_sec=5.0):
             self.get_logger().error('NavigateToPose action server not available after waiting')
             exit(1)
-        self.last_goal = None
         self.initial_pose_publisher = self.create_publisher(PoseWithCovarianceStamped, '/initialpose', 10)
         self.goal_handles = []
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self, spin_thread=True)
     
     def send_initial_pose(self, x, y, theta):
         pose_msg = PoseWithCovarianceStamped()
@@ -60,17 +68,7 @@ class SendNavGoalNode(rclpy.node.Node):
         
         self.initial_pose_publisher.publish(pose_msg)
         
-    def send_nav_goal(self, x, y, theta):
-        # 如果多次以相同的参数调用send_nav_goal，则不会发送新的目标
-        # 2024/06/26: 忽略这部分逻辑
-        if False:
-            x_last, y_last, theta_last = self.last_goal
-            dist = (x-x_last)**2 + (y-y_last)**2
-            rot = (theta-theta_last)**2
-            if dist < DIST_THRESHOLD and rot < ROT_THRESHOLD:
-                return True, 'goal not changed'
-
-        self.last_goal = (x, y, theta)
+    def __send_nav_goal_base(self, x, y, theta):
         q = eular_to_quaternion(0, 0, theta)
 
         goal_msg = NavigateToPose.Goal()
@@ -88,6 +86,10 @@ class SendNavGoalNode(rclpy.node.Node):
         time.sleep(0.1)
         goal_handle = goal_handle_future.result()
         
+        return goal_handle
+        
+    def send_nav_goal(self, x, y, theta):
+        goal_handle = self.__send_nav_goal_base(x, y, theta)
         if not goal_handle.accepted:
             self.get_logger().error('Goal rejected')
             return False, 'goal rejected'
@@ -96,6 +98,32 @@ class SendNavGoalNode(rclpy.node.Node):
         self.goal_handles.append(goal_handle)
         
         return True, 'goal accepted'
+    
+    def send_nav_goal_sync(self, x, y, theta):
+        goal_handle = self.__send_nav_goal_base(x, y, theta)
+        if not goal_handle.accepted:
+            self.get_logger().error('Goal rejected')
+            return False, 'goal rejected'
+        self.get_logger().info('Move started')
+        self.goal_handles.append(goal_handle)
+        
+        # wait for the goal handle to finish
+        not_move_statuses = [GoalStatus.STATUS_SUCCEEDED, GoalStatus.STATUS_CANCELED, GoalStatus.STATUS_ABORTED]
+        import time
+        while goal_handle.status not in not_move_statuses:
+            time.sleep(0.1)
+
+        result_status = goal_handle.status
+        result_status_str = ''
+        if result_status == GoalStatus.STATUS_SUCCEEDED:
+            result_status_str = 'SUCCEEDED'
+        elif result_status == GoalStatus.STATUS_CANCELED:
+            result_status_str = 'CANCELED'
+        elif result_status == GoalStatus.STATUS_ABORTED:
+            result_status_str = 'ABORTED'
+        
+        self.get_logger().info(f'Move finished with status: {result_status_str}')
+        return True, f'move finished with status: {result_status_str}'
     
     def is_moving(self):
         if len(self.goal_handles) == 0:
@@ -110,6 +138,19 @@ class SendNavGoalNode(rclpy.node.Node):
         gh = self.goal_handles[-1]
         gh.cancel_goal()
         return True, 'goal canceled'
+    
+    def robot_loc(self):
+        # 获得ROBOT_FRAME相对于MAP_FRAME的坐标变换（最新可用的）
+        transform = self.tf_buffer.lookup_transform(MAP_FRAME, ROBOT_FRAME, rclpy.time.Time(), rclpy.time.Duration(seconds=5))
+
+        x = transform.transform.translation.x
+        y = transform.transform.translation.y
+
+        q = transform.transform.rotation
+        e = quaternion_to_eular(q.x, q.y, q.z, q.w)
+        theta = e[2]
+
+        return x, y, theta
 
 rclpy.init()
 node = SendNavGoalNode()
@@ -148,6 +189,44 @@ def nav():
         err = RuntimeError(message)
         err.code = 500
         raise err
+    
+@app.route('/nav_sync', methods=['POST'])
+def nav_sync():
+    try:
+        req : NavRequest = NavRequest(
+            x = request.json.get('x'),
+            y = request.json.get('y'),
+            theta = request.json.get('theta'))
+    except ValueError as ve:
+        msg = ve.errors()[0]['msg']
+        err = RuntimeError(msg)
+        err.code = 400
+        raise err
+    x = req.x; y = req.y; theta = req.theta
+
+    # check if the input is valid
+    if x is None or y is None or theta is None:
+        err = RuntimeError('invalid input: x, y, theta must be provided')
+        err.code = 400
+        raise err
+    success, message = node.send_nav_goal_sync(x, y, theta)
+    if success:
+        return jsonify({'status': 'success', 'message': message})
+    else:
+        err = RuntimeError(message)
+        err.code = 500
+        raise err
+
+
+@app.route("/nav_test", methods=['POST'])
+def nav_test():
+    import math
+    import time
+    curX, curY, curTheta = node.robot_loc()
+    node.send_nav_goal_sync(curX, curY, curTheta + math.pi) # 旋转180度
+    time.sleep(1)
+    node.send_nav_goal_sync(curX, curY, curTheta) # 旋转回来
+    return jsonify({'status': 'success', 'message': 'Nav test finished. Please check whether the robot has rotated 180 degrees and then rotated back.'})
 
 @app.route('/initial_pose', methods=['POST'])
 def initial_pose():
